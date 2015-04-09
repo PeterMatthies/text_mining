31819864b31da15ea4e1da9e8c73e2ba0d060f42
Peter Matthies <petruxa0708@gmail.com>
2015-04-09 13:14:40 +0300

    added year to class attributes

--- parse_abstracts.py
+++ parse_abstracts.py
@@ -7,11 +7,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-class cArticle(object):
+class ArticleClass(object):
     def __init__(self):
         self.title = ''
         self.abstract = ''
         self.authors = []
+        self.year = 0
         self.PMID = 0
         self.dictionary = {}
 
@@ -25,12 +26,16 @@ def parse_articles(path):
     for element in root.iter(tag=etree.Element):
         # print("%s - %s" % (element.tag, element.text))
         if element.tag == 'MedlineCitation':
-            article = cArticle()
+            article = ArticleClass()
             for child in element:
                 if child.tag == 'PMID':
                     article.PMID = child.text
                 if child.tag == 'Article':
                     for sub_article in child:
+                        if sub_article.tag == 'Journal':
+                            for journal_child in sub_article:
+                                if journal_child.tag == 'JournalIssue':
+                                    article.year = [a for a in journal_child if a.tag == 'Year'][0]
                         if sub_article.tag == 'ArticleTitle':
                             article.title = sub_article.text
                         if sub_article.tag == 'Abstract':
@@ -54,35 +59,36 @@ start = time()
 
 path = './human_gut_microbiota.xml'
 
-articles_list = parse_articles(path)
+art_list = parse_articles(path)
 
 authors_graph = nx.Graph()
 articles_graph = nx.Graph()
 
-for article in articles_list[:]:
-    authors_graph.add_nodes_from(article.authors)
-    for author_1 in article.authors:
-        for author_2 in article.authors:
+for art in art_list[:]:
+    authors_graph.add_nodes_from(art.authors)
+    for author_1 in art.authors:
+        for author_2 in art.authors:
             if author_1 != author_2:
                 authors_graph.add_edge(author_1, author_2, {'weight': 1})
 
-                # print '_' * 80
-                # print article.title
-                # print 'PMID=', article.PMID
-                # for author in article.authors:
-                #     print author
-                # print article.abstract
-                #
+    print '_' * 80
+    print art.title
+    print art.year
+    print 'PMID=', art.PMID
+    for author in art.authors:
+        print author
+    print art.abstract
 
-for i in range(len(articles_list[:])):
-    for j in range(i+1, len(articles_list[:])):
-        if articles_list[i] != articles_list[j]:
-            articles_graph.add_node(articles_list[i].PMID)
-            articles_graph.add_node(articles_list[j].PMID)
-        articles_graph.add_edge(articles_list[i].PMID, articles_list[j].PMID, {'weight': 1})
-        for author in articles_list[j].authors:
-            if author in articles_list[i].authors:
-                articles_graph[articles_list[i].PMID][articles_list[j].PMID]['weight'] += 1
+
+for i in range(len(art_list[:])):
+    for j in range(i+1, len(art_list[:])):
+        if art_list[i] != art_list[j]:
+            articles_graph.add_node(art_list[i].PMID)
+            articles_graph.add_node(art_list[j].PMID)
+        articles_graph.add_edge(art_list[i].PMID, art_list[j].PMID, {'weight': 1})
+        for author in art_list[j].authors:
+            if author in art_list[i].authors:
+                articles_graph[art_list[i].PMID][art_list[j].PMID]['weight'] += 1
 
 
 for edge in articles_graph.edges():
