78cdb8a32907107092865b9e986014b47fe7c65d
Peter Matthies <petruxa0708@gmail.com>
2015-04-14 17:04:33 +0300

    bacteria disease interactions

--- bact_disease_interact.py
+++ bact_disease_interact.py
@@ -2,6 +2,7 @@ __author__ = 'petr'
 
 
 from time import time
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from parse_abstracts import parse_articles
@@ -19,44 +20,94 @@ for art in art_list[:]:
     art.get_bacteria()
     art.get_diseases()
 
+bact = []
+diss = []
+
 for article in art_list:
-    if article.bacteria is not None:
+    if len(article.bacteria) > 0:
         for bacteria in article.bacteria:
-            bact_diseases_graph.add_node(bacteria[1][0][0])
-            bact_diseases_graph.node[bacteria[1][0][0]]['weight'] = 0
-    if article.disease_names is not None:
+            for b in bacteria[1]:
+                if b[0] not in bact:
+                    bact.append(b[0])
+                bact_diseases_graph.add_node(b[0])
+                bact_diseases_graph.node[b[0]]['weight'] = 0
+    if len(article.disease_names) > 0:
         for disease in article.disease_names:
+            if disease not in diss:
+                diss.append(disease)
             bact_diseases_graph.add_node(disease)
             bact_diseases_graph.node[disease]['weight'] = 1
-
-
 for article in art_list:
-    if article.bacteria is not None:
+    if len(article.bacteria) > 0:
         for bacteria in article.bacteria:
-            bact_diseases_graph.node[bacteria[1][0][0]]['weight'] += bacteria[1][0][1]
-            bact_diseases_graph.node[bacteria[1][0][0]]['color'] = 'r'
-    if article.disease_names is not None:
+            for b in bacteria[1]:
+                bact_diseases_graph.node[b[0]]['weight'] += b[1]
+                bact_diseases_graph.node[b[0]]['color'] = 'r'
+    if len(article.disease_names) > 0:
         for disease_id, disease in zip(article.disease_ids, article.disease_names):
             bact_diseases_graph.node[disease]['weight'] += article.disease_ids[disease_id]
             bact_diseases_graph.node[disease]['color'] = 'b'
 
-    if article.bacteria and article.disease_names is not None:
+    if len(article.bacteria) and len(article.disease_names) > 0:
         for bacteria in article.bacteria:
-            for disease in article.disease_names:
-                bact_diseases_graph.add_edge(bacteria[1][0][0], disease)
+            for b in bacteria[1]:
+                for disease in article.disease_names:
+                    bact_diseases_graph.add_edge(b[0], disease)
+                    if 'weight' in bact_diseases_graph[b[0]][disease]:
+                        bact_diseases_graph[b[0]][disease]['weight'] += 1
+                    else:
+                        bact_diseases_graph[b[0]][disease]['weight'] = 1
+
 
 for node in bact_diseases_graph.nodes():
     if len(bact_diseases_graph.neighbors(node)) == 0:
         bact_diseases_graph.remove_node(node)
 
+
 bact_diseases_graph.remove_node('disease')
 bact_diseases_graph.remove_node('syndrome')
+pos = nx.spring_layout(bact_diseases_graph)
 sizes = [bact_diseases_graph.node[node]['weight']*10 for node in bact_diseases_graph.nodes()]
 colors = [bact_diseases_graph.node[node]['color'] for node in bact_diseases_graph.nodes()]
-print sizes
-nx.draw(bact_diseases_graph, node_size=sizes, node_color=colors)
+labels = {edge: bact_diseases_graph[edge[0]][edge[1]]['weight'] for edge in bact_diseases_graph.edges()}
+
+max_, max_node = 0, 0
+for node in bact_diseases_graph.nodes():
+    neighbors_len = len(bact_diseases_graph.neighbors(node))
+    if max_ < neighbors_len:
+        max_ = neighbors_len
+        max_node = node
+print max_, max_node
+
+nx.draw(bact_diseases_graph, pos, node_size=sizes, node_color=colors)
+nx.draw_networkx_edge_labels(bact_diseases_graph, pos, edge_labels=labels)
 plt.show()
 
-end = time()
 
+# bact_diseases_matrix = np.zeros((len(bact), len(diss)))
+# print bact
+# print diss
+# for i, bac in enumerate(bact):
+#     for j, dis in enumerate(diss):
+#         if (bac, dis) in bact_diseases_graph.edges():
+#             bact_diseases_matrix[i][j] = labels[(bac, dis)]
+# print bact_diseases_matrix.shape
+
+# with open('bact_diseases.txt', 'w') as f:
+#     for edge in bact_diseases_graph.edges():
+#         f.write('\t'.join((edge[0], 'bact_disease', edge[1])) + '\n')
+
+
+# with open('bact_diseases_matrix.txt', 'w') as f:
+#     f.write('' + '\t' + '\t'.join(diss) + '\n')
+#     for bact, row in zip(bact, bact_diseases_matrix):
+#             row = [str(elem) for elem in row]
+#             f.write(bact+ '\t' + '\t'.join(row) + '\n')
+
+
+end = time()
 print end - start
+
+
+
+
