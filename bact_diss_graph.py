2265fc207415419759e12baa658c907db498674c
Peter Matthies <petruxa0708@gmail.com>
2015-06-10 12:59:02 +0300

    commit 10.06.15

--- bact_diss_graph.py
+++ bact_diss_graph.py
@@ -3,6 +3,7 @@ __author__ = 'petr'
 from time import time
 import igraph as ig
 from parse_abstracts import parse_articles
+import matplotlib.pyplot as plt
 
 start = time()
 
@@ -11,7 +12,7 @@ path = './human_gut_microbiota.xml'
 art_list = parse_articles(path)
 
 bact_diseases_graph = ig.Graph()
-
+authors_bd = ig.Graph()
 for art in art_list[:]:
     art.get_bacteria()
     art.get_diseases()
@@ -30,18 +31,19 @@ for article in art_list:
         for bacteria in article.bacteria:
             for b in bacteria[1]:
                 if 'name' not in bact_diseases_graph.vs.attributes():
-                    bact_diseases_graph.add_vertex(b[0])
-                    vertex_weights[b[0]] = b[1]
+                    bact_diseases_graph.add_vertex(b[0].encode('utf8'))
+                    vertex_weights[b[0].encode('utf8')] = b[1]
                     vertex_types.append('bacteria')
                 else:
-                    if b[0] not in bact_diseases_graph.vs['name']:
-                        bact_diseases_graph.add_vertex(b[0])
-                        vertex_weights[b[0]] = b[1]
+                    if b[0].encode('utf8') not in bact_diseases_graph.vs['name']:
+                        bact_diseases_graph.add_vertex(b[0].encode('utf8'))
+                        vertex_weights[b[0].encode('utf8')] = b[1]
                         vertex_types.append('bacteria')
                     else:
-                        vertex_weights[b[0]] += b[1]
+                        vertex_weights[b[0].encode('utf8')] += b[1]
     if len(article.disease_names) > 0:
         for disease_id, disease in zip(article.disease_ids, article.disease_names):
+            disease = disease.encode('utf8')
             if disease != 'disease' and 'syndrome':
                 if 'name' not in bact_diseases_graph.vs.attributes():
                     bact_diseases_graph.add_vertex(disease)
@@ -55,29 +57,125 @@ for article in art_list:
                     else:
                         vertex_weights[disease] += article.disease_ids[disease_id]
     if len(article.bacteria) and len(article.disease_names) > 0:
+        for author in article.authors:
+            author = author.encode('utf8')
+            if 'name' not in bact_diseases_graph.vs.attributes():
+                bact_diseases_graph.add_vertex(author)
+                vertex_weights[author] = 1
+                vertex_types.append('author')
+            else:
+                if author not in bact_diseases_graph.vs['name']:
+                    bact_diseases_graph.add_vertex(author)
+                    vertex_weights[author] = 1
+                    vertex_types.append('author')
+                else:
+                    vertex_weights[author] += 1
         for bacteria in article.bacteria:
             for b in bacteria[1]:
                 for disease in article.disease_names:
                     if disease != 'disease' and 'syndrome':
-                        if not bact_diseases_graph.are_connected(b[0], disease):
-                            bact_diseases_graph.add_edge(b[0], disease)
-                            edges_weights[' '.join((b[0], disease))] = 1
+                        if not bact_diseases_graph.are_connected(disease, b[0].encode('utf8')):
+                            bact_diseases_graph.add_edge(disease, b[0].encode('utf8'))
+                            edges_weights['_'.join((disease, b[0].encode('utf8')))] = 1
                         else:
-                            edges_weights[' '.join((b[0], disease))] += 1
+                            edges_weights['_'.join((disease, b[0].encode('utf8')))] += 1
+                        for author in article.authors:
+                            author = author.encode('utf8')
+                            if author == 'Seganti L':
+                                print "he is here", disease, b[0]
+                            if not bact_diseases_graph.are_connected(author, disease):
+                                bact_diseases_graph.add_edge(author, disease)
+                                edges_weights['_'.join((author, disease))] = 1
+                            else:
+                                edges_weights['_'.join((author, disease))] += 1
+                            if not bact_diseases_graph.are_connected(author, b[0].encode('utf8')):
+                                bact_diseases_graph.add_edge(author, b[0].encode('utf8'))
+                                edges_weights['_'.join((author, b[0].encode('utf8')))] = 1
+                            else:
+                                edges_weights['_'.join((author, b[0].encode('utf8')))] += 1
 
-
-print bact_diseases_graph.vs['name']
-
-print vertex_types
-print vertex_weights
-print edges_weights
+#
+# print bact_diseases_graph.vs['name']
+#
+# print vertex_types
+# print vertex_weights
+# print edges_weights
 bact_diseases_graph.vs['type'] = vertex_types
-color_dict = {'bacteria': 'red', 'disease': 'blue'}
+color_dict = {'bacteria': 'red', 'disease': 'blue', 'author': 'green'}
 bact_diseases_graph.vs['color'] = [color_dict[type_] for type_ in bact_diseases_graph.vs['type']]
 bact_diseases_graph.vs['label'] = bact_diseases_graph.vs['name']
 
-l1 = bact_diseases_graph.layout('circular')
-ig.plot(bact_diseases_graph, layout=l1, bbox=(4000, 4000), margin=20)
 
+print len(vertex_weights), len(edges_weights)
+all_bacteria = list(set([b[0].encode('utf8') for article in art_list
+                         for bacteria in article.bacteria for b in bacteria[1]
+                         if len(article.disease_names) > 0 and len(article.bacteria) > 0]))
+all_authors = list(set([author.encode('utf8') for article in art_list for author in article.authors
+                        if len(article.disease_names) > 0 and len(article.bacteria) > 0]))
+all_diseases = list(set([disease.encode('utf8') for article in art_list for disease in article.disease_names
+                         if len(article.disease_names) > 0 and len(article.bacteria) > 0]))
+
+
+print all_authors
+print all_bacteria
+print all_diseases
+
+
+for a in all_authors:
+    for bacteria in all_bacteria:
+        if '_'.join((a, bacteria)) not in edges_weights.keys():
+            edges_weights['_'.join((a, bacteria))] = 0
+
+    for d in all_diseases:
+        if '_'.join((a, d)) not in edges_weights.keys():
+            edges_weights['_'.join((a, d))] = 0
+
+for d in all_diseases:
+    for bacteria in all_bacteria:
+        if '_'.join((d, bacteria)) not in edges_weights.keys():
+            edges_weights['_'.join((d, bacteria))] = 0
+
+
+b_a = []
+b_d = []
+a_d = []
+# for bacteria in all_bacteria:
+#     b_a.append(sum([edges_weights['_'.join((a, bacteria))] for a in all_authors]))
+#     b_d.append(sum([edges_weights['_'.join((d, bacteria))] for d in all_diseases]))
+
+for a in all_authors:
+    b_a.append(sum([edges_weights['_'.join((a, b))] for b in all_bacteria]))
+    a_d.append(sum([edges_weights['_'.join((a, d))] for d in all_diseases]))
+
+with open('authors_bd.txt', 'w') as f:
+    for a, x, y in zip(all_authors, b_a, a_d):
+        f.write(a + '\t' + '\t'.join((str(x), str(y))) + '\n')
+
+a_d = []
+d_b = []
+
+for disease in all_diseases:
+    a_d.append(sum([edges_weights['_'.join((a, d))] for a in all_authors]))
+    d_b.append(sum([edges_weights['_'.join((d, b))] for b in all_bacteria]))
+
+
+with open('diseases_ab.txt', 'w') as f:
+    for d, x, y in zip(all_diseases, a_d, d_b):
+        f.write(d + '\t' + '\t'.join((str(x), str(y))) + '\n')
+
+
+plt.scatter(b_a, a_d)
+plt.show()
+
+plt.scatter(a_d, d_b)
+plt.show()
+
+
+#
+# l1 = bact_diseases_graph.layout('circular')
+# l2 = bact_diseases_graph.layout('kk')
+# plot = ig.plot(bact_diseases_graph, layout=l1, bbox=(6000, 6000), margin=20)
+#
+# plot.save('auth_bact_dis_graph4.png')
 end = time()
 print end - start
