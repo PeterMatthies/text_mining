3d97e67cd28a83c1005486449768f5941f970118
Peter Matthies <petruxa0708@gmail.com>
2015-06-11 18:25:31 +0300

    commit 11.06.15, recalculated interaction abd graphs

--- bact_diss_graph.py
+++ bact_diss_graph.py
@@ -27,7 +27,20 @@ edges_weights = {}
 vertex_types = []
 
 for article in art_list:
-    if len(article.bacteria) > 0:
+    for author in article.authors:                      # adding authors from current article to the graph
+            author = author.encode('utf8')
+            if 'name' not in bact_diseases_graph.vs.attributes():
+                bact_diseases_graph.add_vertex(author)
+                vertex_weights[author] = 1
+                vertex_types.append('author')
+            else:
+                if author not in bact_diseases_graph.vs['name']:
+                    bact_diseases_graph.add_vertex(author)
+                    vertex_weights[author] = 1
+                    vertex_types.append('author')
+                else:
+                    vertex_weights[author] += 1
+    if len(article.bacteria) > 0:                       # adding bacteria from current article to the graph
         for bacteria in article.bacteria:
             for b in bacteria[1]:
                 if 'name' not in bact_diseases_graph.vs.attributes():
@@ -41,7 +54,15 @@ for article in art_list:
                         vertex_types.append('bacteria')
                     else:
                         vertex_weights[b[0].encode('utf8')] += b[1]
-    if len(article.disease_names) > 0:
+                for author in article.authors:          # adding edges between authors and bacteria
+                    author = author.encode('utf8')
+                    if not bact_diseases_graph.are_connected(author, b[0].encode('utf8')):
+                        bact_diseases_graph.add_edge(author, b[0].encode('utf8'))
+                        edges_weights['_'.join((author, b[0].encode('utf8')))] = 1
+                    else:
+                        edges_weights['_'.join((author, b[0].encode('utf8')))] += 1
+
+    if len(article.disease_names) > 0:                  # adding diseases from current article to the graph
         for disease_id, disease in zip(article.disease_ids, article.disease_names):
             disease = disease.encode('utf8')
             if disease != 'disease' and 'syndrome':
@@ -56,20 +77,14 @@ for article in art_list:
                         vertex_types.append('disease')
                     else:
                         vertex_weights[disease] += article.disease_ids[disease_id]
-    if len(article.bacteria) and len(article.disease_names) > 0:
-        for author in article.authors:
-            author = author.encode('utf8')
-            if 'name' not in bact_diseases_graph.vs.attributes():
-                bact_diseases_graph.add_vertex(author)
-                vertex_weights[author] = 1
-                vertex_types.append('author')
-            else:
-                if author not in bact_diseases_graph.vs['name']:
-                    bact_diseases_graph.add_vertex(author)
-                    vertex_weights[author] = 1
-                    vertex_types.append('author')
-                else:
-                    vertex_weights[author] += 1
+                for author in article.authors:           # adding edges between authors and diseases
+                    author = author.encode('utf8')
+                    if not bact_diseases_graph.are_connected(author, disease):
+                        bact_diseases_graph.add_edge(author, disease)
+                        edges_weights['_'.join((author, disease))] = 1
+                    else:
+                        edges_weights['_'.join((author, disease))] += 1
+    if len(article.bacteria) and len(article.disease_names) > 0:   # adding edges between bacteria and diseases
         for bacteria in article.bacteria:
             for b in bacteria[1]:
                 for disease in article.disease_names:
@@ -79,20 +94,7 @@ for article in art_list:
                             edges_weights['_'.join((disease, b[0].encode('utf8')))] = 1
                         else:
                             edges_weights['_'.join((disease, b[0].encode('utf8')))] += 1
-                        for author in article.authors:
-                            author = author.encode('utf8')
-                            if author == 'Seganti L':
-                                print "he is here", disease, b[0]
-                            if not bact_diseases_graph.are_connected(author, disease):
-                                bact_diseases_graph.add_edge(author, disease)
-                                edges_weights['_'.join((author, disease))] = 1
-                            else:
-                                edges_weights['_'.join((author, disease))] += 1
-                            if not bact_diseases_graph.are_connected(author, b[0].encode('utf8')):
-                                bact_diseases_graph.add_edge(author, b[0].encode('utf8'))
-                                edges_weights['_'.join((author, b[0].encode('utf8')))] = 1
-                            else:
-                                edges_weights['_'.join((author, b[0].encode('utf8')))] += 1
+
 
 #
 # print bact_diseases_graph.vs['name']
@@ -100,6 +102,7 @@ for article in art_list:
 # print vertex_types
 # print vertex_weights
 # print edges_weights
+
 bact_diseases_graph.vs['type'] = vertex_types
 color_dict = {'bacteria': 'red', 'disease': 'blue', 'author': 'green'}
 bact_diseases_graph.vs['color'] = [color_dict[type_] for type_ in bact_diseases_graph.vs['type']]
@@ -107,6 +110,8 @@ bact_diseases_graph.vs['label'] = bact_diseases_graph.vs['name']
 
 
 print len(vertex_weights), len(edges_weights)
+
+# getting lists of all authors, bacteria and diseases from all articles
 all_bacteria = list(set([b[0].encode('utf8') for article in art_list
                          for bacteria in article.bacteria for b in bacteria[1]
                          if len(article.disease_names) > 0 and len(article.bacteria) > 0]))
@@ -121,7 +126,7 @@ print all_bacteria
 print all_diseases
 
 
-for a in all_authors:
+for a in all_authors:                        # setting weights to zero
     for bacteria in all_bacteria:
         if '_'.join((a, bacteria)) not in edges_weights.keys():
             edges_weights['_'.join((a, bacteria))] = 0
@@ -136,40 +141,51 @@ for d in all_diseases:
             edges_weights['_'.join((d, bacteria))] = 0
 
 
-b_a = []
-b_d = []
-a_d = []
-# for bacteria in all_bacteria:
-#     b_a.append(sum([edges_weights['_'.join((a, bacteria))] for a in all_authors]))
-#     b_d.append(sum([edges_weights['_'.join((d, bacteria))] for d in all_diseases]))
+bact_auth = []
+bact_dis = []
+auth_dis = []
 
-for a in all_authors:
-    b_a.append(sum([edges_weights['_'.join((a, b))] for b in all_bacteria]))
-    a_d.append(sum([edges_weights['_'.join((a, d))] for d in all_diseases]))
+for bacteria in all_bacteria:                 # bacteria from diseases, authors
+    bact_auth.append(sum([edges_weights['_'.join((a, bacteria))] for a in all_authors]))
+    bact_dis.append(sum([edges_weights['_'.join((d, bacteria))] for d in all_diseases]))
 
-with open('authors_bd.txt', 'w') as f:
-    for a, x, y in zip(all_authors, b_a, a_d):
+with open('bacteria_ad_1.txt', 'w') as f:
+    for b, x, y in zip(all_bacteria, bact_auth, bact_dis):
+        f.write(b + '\t' + '\t'.join((str(x), str(y))) + '\n')
+
+bact_auth = []
+bact_dis = []
+auth_dis = []
+
+
+for a in all_authors:                         # authors from bacteria, diseases
+    bact_auth.append(sum([edges_weights['_'.join((a, b))] for b in all_bacteria]))
+    auth_dis.append(sum([edges_weights['_'.join((a, d))] for d in all_diseases]))
+
+with open('authors_bd_1.txt', 'w') as f:
+    for a, x, y in zip(all_authors, bact_auth, auth_dis):
         f.write(a + '\t' + '\t'.join((str(x), str(y))) + '\n')
 
-a_d = []
-d_b = []
+bact_auth = []
+bact_dis = []
+auth_dis = []
 
-for disease in all_diseases:
-    a_d.append(sum([edges_weights['_'.join((a, d))] for a in all_authors]))
-    d_b.append(sum([edges_weights['_'.join((d, b))] for b in all_bacteria]))
+for disease in all_diseases:                  # diseases from authors, bacteria
+    auth_dis.append(sum([edges_weights['_'.join((a, d))] for a in all_authors]))
+    bact_dis.append(sum([edges_weights['_'.join((d, b))] for b in all_bacteria]))
 
 
-with open('diseases_ab.txt', 'w') as f:
-    for d, x, y in zip(all_diseases, a_d, d_b):
+with open('diseases_ab_1.txt', 'w') as f:
+    for d, x, y in zip(all_diseases, auth_dis, bact_dis):
         f.write(d + '\t' + '\t'.join((str(x), str(y))) + '\n')
 
 
-plt.scatter(b_a, a_d)
-plt.show()
-
-plt.scatter(a_d, d_b)
-plt.show()
-
+# plt.scatter(b_a, a_d)
+# plt.show()
+#
+# plt.scatter(a_d, d_b)
+# plt.show()
+#
 
 #
 # l1 = bact_diseases_graph.layout('circular')
